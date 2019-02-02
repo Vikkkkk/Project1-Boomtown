d0cffb73ee0cd7cac5172a590e56821c10e3583d
Vikkkkk <vicguo.99@gmail.com>
2019-02-02 15:51:56 -0800

    using regular expression to ensure email format is valid

--- client/src/components/AccountForm/helpers/validation.js
+++ client/src/components/AccountForm/helpers/validation.js
@@ -1,15 +1,17 @@
-export default function validate(values, auth) {
+export default function validate(values, userlogin) {
   console.log(values);
   const errors = {};
   if (!values.email) {
     console.log('emailfked');
-    errors.email = 'Email is fked';
+    errors.email = 'Email is Required';
+  } else if (/.*@.*\..*/.test(values.email) === false) {
+    errors.email = 'Email is Invalid';
   }
   if (!values.password) {
     console.log('pwfked');
-    errors.password = 'Password is fked ';
+    errors.password = 'Password is Required ';
   }
-  if (!auth && !values.fullname) {
+  if (!userlogin && !values.fullname) {
     errors.fullname = 'Name is Required';
   }
   return errors;
